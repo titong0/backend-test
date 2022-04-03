9893d0791225fd73cb95aa6a3c0eda72caaf3631
titong0 <augustoerrecarte@gmail.com>
2022-04-03 11:40:20 -0300

    fix please

--- api/config.js
+++ api/config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "./.env") });
 
-const host = process.env.HOST || "http://localhost";
+const host = process.env.HOST || "0.0.0.0";
 const port = process.env.PORT || 3000;
 const db = process.env.DB || "mongodb://localhost/no-se";
 
--- api/src/index.js
+++ api/src/index.js
@@ -1,5 +1,6 @@
 const fastify = require("fastify")({ logger: true });
 const productsRoutes = require("./routes/products.routes");
+const config = require("../config");
 require("./utils/mongoose");
 
 fastify.get("/", (req, res) => {
@@ -9,7 +10,7 @@ fastify.get("/", (req, res) => {
 productsRoutes.forEach((route) => fastify.route(route));
 
 const start = async () => {
-  await fastify.listen(process.env.PORT);
+  await fastify.listen(config.port);
   fastify.log.info(`server listening on ${fastify.server.address()}`);
 };
 
