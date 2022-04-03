0db17f782fc7026c0bc254b39b9e680aab4ba4e0
titong0 <augustoerrecarte@gmail.com>
2022-04-03 11:44:42 -0300

    fix.

--- api/src/index.js
+++ api/src/index.js
@@ -1,4 +1,4 @@
-const fastify = require("fastify")({ logger: true });
+const fastify = require("fastify").default({ logger: true });
 const productsRoutes = require("./routes/products.routes");
 const config = require("../config");
 require("./utils/mongoose");
@@ -6,11 +6,10 @@ require("./utils/mongoose");
 fastify.get("/", (req, res) => {
   res.send({ hola: "no" });
 });
-
 productsRoutes.forEach((route) => fastify.route(route));
 
 const start = async () => {
-  await fastify.listen(config.port);
+  await fastify.listen(config.port, config.host);
   fastify.log.info(`server listening on ${fastify.server.address()}`);
 };
 
